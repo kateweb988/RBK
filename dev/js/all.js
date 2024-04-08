6793bfe6a2dff43f712c01e213847fe025140418
kateweb988 <katyavov2805@gmail.com>
2024-04-08 15:04:41 +0300

    fin

--- dev/js/all.js
+++ dev/js/all.js
@@ -34,7 +34,53 @@ window.addEventListener("DOMContentLoaded", function () {
   });
 
 });
+document.addEventListener("DOMContentLoaded", () => {
+  //popup1
+  let popupBg = document.querySelector('.popup__bg');
+  let popup = document.querySelector('.popup');
+  let openPopupButtons = document.querySelectorAll('.nav__btn');
+  let closePopupButton = document.querySelector('.close-popup');
 
+  openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      popupBg.classList.add('active');
+      popup.classList.add('active');
+    })
+  });
+
+  closePopupButton.addEventListener('click', () => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+  });
+
+  document.addEventListener('click', (e) => {
+    if (e.target === popupBg) {
+      popupBg.classList.remove('active');
+      popup.classList.remove('active');
+    }
+  });
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+      //ваша функция закрытия окна
+      popupBg.classList.remove('active');
+      popup.classList.remove('active');
+    }
+  });
+  document.addEventListener('click', (e) => {
+    if (e.target === popupBg) {
+      popupBg.classList.remove('active');
+      popup.classList.remove('active');
+    }
+  });
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+      //ваша функция закрытия окна
+      popupBg3.classList.remove('active');
+      popup3.classList.remove('active');
+    }
+  });
+});
 document.addEventListener('DOMContentLoaded', function () {
   const swiper = new Swiper('.swiper1', {
     slidesPerView: 1,
